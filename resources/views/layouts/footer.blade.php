c34804353ba8f1fb6437afd3af341f027f9fd333
Sabuz888 <mdsabuz5525@gmail.com>
2023-11-26 23:11:51 +0600

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,16 @@
+<div class="colorlib-footer">
+<br>
+    <ul>
+        <li><a href="https://www.facebook.com/mahadyhasan.hasan.3363" target="_blank"><i class="icon-facebook2"></i></a></li>
+        <li><a href="https://github.com/sabuz888" target="_blank"><i class="icon-github"></i></a></li>
+        <li><a href="https://www.linkedin.com/in/mdsabuz/" target="_blank"><i class="icon-linkedin2"></i></a></li>
+    </ul>
+
+    <br>
+
+    <p>
+        <small>&copy; Copyright <script>document.write(new Date().getFullYear());</script> All rights reserved. Made with 
+            <i class="icon-heart" aria-hidden="true"></i> by Mahady Hasan Sabuz
+        </small>
+    </p>
+</div>
